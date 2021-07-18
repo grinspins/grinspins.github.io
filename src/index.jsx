9b5fd18d873e3a3bedf16bf25e248ee39ec4a966
grinspins <sebastian.mainberger@gmail.com>
2021-07-18 21:26:00 +0200

    Bug: Fix id selector.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 import { Resume } from './components/main'
 import './styles.css'
 
-const container = document.getElementById('#app')
+const container = document.getElementById('app')
 ReactDOM.render(<Resume />, container)
 
