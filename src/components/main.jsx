77b871e17fdf9837c67913a6830c4a661f204821
grinspins <sebastian.mainberger@gmail.com>
2021-07-18 20:51:07 +0200

    MNT: Basic scaffolding.

---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Resume = () => {
+
+  return (
+    <div className="root">
+      This is cool bro
+    </div>
+  )
+
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { Resume } from './components/main'
+import './styles.css'
+
+const container = document.getElementById('#app')
+ReactDOM.render(<Resume />, container)
+
