9483bc878ffe8b52182f1eb6f7941a24f0a0a5a1
grinspins <sebastian.mainberger@gmail.com>
2022-07-23 19:38:14 +0200

    ENH: Theme animation.

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import PropTypes from 'prop-types'
 
 export const Toolbar = ({ theme, setTheme }) => {
+
+  const angle = useRef(0)
 
   const toggleTheme = () => {
     // const icon = document.getElementById("theme-icon")
@@ -11,20 +13,27 @@ export const Toolbar = ({ theme, setTheme }) => {
     setTheme(currentTheme => (
       currentTheme === "light" ? 'dark': 'light'
     ))
+    angle.current += 180;
+    const rotor = document.getElementById("rotor")
+    const icons = document.getElementsByClassName("theme-icon")
+    // rotor.animate({ transform: 'rotate(180deg)'}, 1)
+    rotor.style.transform = `rotate(${angle.current}deg)`
+    for (const icon of icons) {
+      icon.style.transform = `rotate(-${angle.current}deg)`
+    }
+
   }
 
   return (
     <div className='toolbar'>
-      <div className="rotor">
+      <div id="rotor" className="rotor">
         <FontAwesomeIcon
-          id="theme-icon"
-          className={`icon icon-${theme === 'light' ? 'show' : 'hide'}`}
+          className='theme-icon icon-light'
           icon={faSun}
           onClick={() => toggleTheme()}
         />
         <FontAwesomeIcon
-          id="theme-icon"
-          className={`icon icon-${theme === 'dark' ? 'show' : 'hide'}`}
+          className='theme-icon icon-dark'
           icon={faMoon}
           onClick={() => toggleTheme()}
         />
