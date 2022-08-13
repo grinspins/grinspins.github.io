af70aa5468b3465a8449b53101d60e55d7553c3d
grinspins <sebastian.mainberger@gmail.com>
2022-08-13 21:51:09 +0200

    Prettier.

--- test/test-print-url.jsx
+++ test/test-print-url.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import {render, act, fireEvent} from "@testing-library/react"
+import { render, act, fireEvent } from "@testing-library/react";
 import { Toolbar } from "../src/components/toolbar";
-import {printExternal} from "../src/lib/util"
+import { printExternal } from "../src/lib/util";
 
 jest.mock("../src/lib/util", () => ({
-  printExternal: jest.fn()
-}))
+  printExternal: jest.fn(),
+}));
 
 it("Uses the correct print url", () => {
   act(() => {
-    render(<Toolbar setTheme={() => {}} />)
-  })
-  const printIcon = document.getElementsByClassName("icon-print")[0]
-  fireEvent.click(printIcon)
-  const printCallArgs = printExternal.mock.calls[0]
-  expect(printCallArgs[0]).toBe("https://grinspins.github.io/resume.html")
-})
+    render(<Toolbar setTheme={() => {}} />);
+  });
+  const printIcon = document.getElementsByClassName("icon-print")[0];
+  fireEvent.click(printIcon);
+  const printCallArgs = printExternal.mock.calls[0];
+  expect(printCallArgs[0]).toBe("https://grinspins.github.io/resume.html");
+});
